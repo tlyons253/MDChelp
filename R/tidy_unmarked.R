8449e3c4677a875c68d543321c0ccc46e9bc2337
tlyons253 <tlyons253@gmail.com>
2026-04-28 11:34:52 -0500

    tidy function for unmarked fit obj, following naming pattern from Ken Kellner

--- R/tidy_unmarked.R
+++ R/tidy_unmarked.R
@@ -1,4 +1,7 @@
-#' "tidy" object of unmarked parameter estimates
+
+
+
+#' tidy unmarked parameter estimates
 #'
 #' @param X an unmarked fit object
 #'
@@ -6,24 +9,18 @@
 #'   does for lm/glm objects
 #'
 #' @export
+tidy.unmarkedFit<-function(X){
+  X@estimates->param.list
+
+  map(param.list,~um.help(.x))%>%bind_rows()->tmp
+
+  row.names(tmp)<-NULL
+
+  return(tmp)}
 #'
 #' @examples
-#'tidy.um<-function(X){
-#'  X@estimates->param.list
 
-
-#'  #
-#'  map(param.list,~um.help(.x))%>%
-#'    bind_rows()->tmp
-
-#'  row.names(tmp)<-NULL
-
-
-#'  return(tmp)
-
-
-#'}
-
+'tidy.umarkedFit'
 
 
 #
@@ -35,17 +32,18 @@
 #'   values (assuming normal distribution)
 #'
 #' @export
+ um.help<-function(X){
+data.frame(process=rep(X@name,length(X@estimates)),
+           shorty=rep(X@short.name,length(X@estimates)),
+           estimate=X@estimates,
+           SE=sqrt(diag(X@covMat)))%>%
+  mutate(approximate.Z=estimate/SE,
+         approximate.P=pnorm(abs(approximate.Z),
+                             lower.tail=FALSE)*2)%>%
+  rownames_to_column(var='term')%>%
+  unite(col='parameter',shorty,term)%>%
+  relocate(process,parameter)
+}
 #'
 #' @examples
-#'um.help<-function(X){
-#'  data.frame(process=rep(X@name,length(X@estimates)),
-#'             shorty=rep(X@short.name,length(X@estimates)),
-#'             estimate=X@estimates,
-#'             SE=sqrt(diag(X@covMat)))%>%
-#'    mutate(approximate.Z=estimate/SE,
-#'           approximate.P=pnorm(abs(approximate.Z),
-#'                               lower.tail=FALSE)*2)%>%
-#'    rownames_to_column(var='term')%>%
-#'    unite(col='parameter',shorty,term)%>%
-#'    relocate(process,parameter)
-#'}
+'um.help'
